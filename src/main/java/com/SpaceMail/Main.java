d3f14a2f5f3a857beed7a0303cabeebcee7ea47a
Juan Pablo Gargiulo <juanpab84@gmail.com>
2016-11-16 17:10:52 -0300

    Commit Inicial

---
+++ src/main/java/com/SpaceMail/Main.java
@@ -0,0 +1,16 @@
+package com.SpaceMail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by juan on 10/31/16.
+ */
+@SpringBootApplication
+public class Main {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class    , args);
+    }
+}
---
+++ src/main/java/com/SpaceMail/configuracion.java
@@ -0,0 +1,13 @@
+package com.SpaceMail;
+
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * Created by juan on 11/2/16.
+ */
+@Configuration
+public class configuracion {
+
+
+}
